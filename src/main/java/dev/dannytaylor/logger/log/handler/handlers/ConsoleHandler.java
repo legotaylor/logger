3ba70d415f68ff489ca723dd5ce521d39e293ea7
dannytaylor <daniel@mclegoman.com>
2025-05-16 09:57:54 +1200

    console should be able to use colors.

--- src/main/java/dev/dannytaylor/logger/log/handler/handlers/ConsoleHandler.java
+++ src/main/java/dev/dannytaylor/logger/log/handler/handlers/ConsoleHandler.java
@@ -3,11 +3,25 @@ package dev.dannytaylor.logger.log.handler.handlers;
 import dev.dannytaylor.logger.log.Logger;
 import dev.dannytaylor.logger.log.handler.Handler;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConsoleHandler extends Handler {
+	private final Map<String, Color> styles = new HashMap<>();
 	public void log(Logger.Type type, String message) {
 		log(format(type, message), "");
 	}
 	public void log(String log, String style) {
-		System.out.println(log);
+		System.out.println(ansiColor(styles.get(style)) + log + "\u001B[0m");
+	}
+	public void addStyle(String id, Color color) {
+		styles.put(id, color);
+	}
+	public Map<String, Color> getStyles() {
+		return styles;
+	}
+	public String ansiColor(Color color) {
+		return "\u001B[38;2;" + color.getRed() + ";" + color.getGreen() + ";" + color.getBlue() + "m";
 	}
 }
